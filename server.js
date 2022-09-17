942e58f7dc8816bf301e8b7104d4254c6ec64ae0
Jimmy Solis <jimmysolis14@gmail.com>
2022-09-17 19:27:44 -0400

    i have added all the needed things for my server.js file.

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = requires('./routes')
+const routes = requires('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: ture}));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server runnning on ${PORT} 🚨`)
+    })
+});
